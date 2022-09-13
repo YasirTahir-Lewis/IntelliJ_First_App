7cf8c45087be987e43b95346ff71fa8b22e0dcf1
Yasir Tahir <yasirtahir1999@gmail.com>
2022-09-13 14:44:37 -0500

    First commit!

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        Person person = new Person("Yasir");
+        System.out.println(person.getName());
+    }
+}
